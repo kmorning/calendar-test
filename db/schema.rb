12a57251f1de62f9d7dad7ff53cff1253a1a21c3
Kelly Morning <kleemorning@gmail.com>
2016-02-14 14:59:50 -0500

    add title back to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214185259) do
+ActiveRecord::Schema.define(version: 20160214195854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160214185259) do
     t.datetime "end_time"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "title"
   end
 
 end
