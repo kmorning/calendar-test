75caf09afa9281c9f657d422c06e04c962103201
Kelly Morning <kleemorning@gmail.com>
2016-02-28 10:52:02 -0500

    remove time hidden field

--- app/inputs/timepicker_input.rb
+++ app/inputs/timepicker_input.rb
@@ -1,7 +1,7 @@
 class TimepickerInput < SimpleForm::Inputs::Base
   def input
-    @builder.text_field(attribute_name, input_html_options) + \
-    @builder.hidden_field(attribute_name, { :class => attribute_name.to_s + "-alt"})
+    @builder.text_field(attribute_name, input_html_options) #+ \
+    #@builder.hidden_field(attribute_name, { :class => attribute_name.to_s + "-alt"})
   end
 end
 
